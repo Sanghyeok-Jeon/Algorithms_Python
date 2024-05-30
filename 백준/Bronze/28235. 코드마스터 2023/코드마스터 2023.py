c0dd45e72e1c16f15a2f8ad0f75bd699191ecf66
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-05-30 11:19:10 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+fighting = input()
+
+if fighting == 'SONGDO':
+    print('HIGHSCHOOL')
+elif fighting == 'CODE':
+    print('MASTER')
+elif fighting == '2023':
+    print('0611')
+elif fighting == 'ALGORITHM':
+    print('CONTEST')
