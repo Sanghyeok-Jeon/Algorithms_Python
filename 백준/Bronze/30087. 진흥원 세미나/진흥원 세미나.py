f5cc7439dd7013a5b4068e341a0ee69c1b5bbb35
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-05-30 11:21:46 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,18 @@
+N = int(input())
+for _ in range(N):
+    classroom = input()
+
+    if classroom == 'Algorithm':
+        print(204)
+    elif classroom == 'DataAnalysis':
+        print(207)
+    elif classroom == 'ArtificialIntelligence':
+        print(302)
+    elif classroom == 'CyberSecurity':
+        print('B101')
+    elif classroom == 'Network':
+        print(303)
+    elif classroom == 'Startup':
+        print(501)
+    elif classroom == 'TestStrategy':
+        print(105)
