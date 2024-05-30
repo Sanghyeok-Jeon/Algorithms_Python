112e4edbce8daa03baeb96e8256e52b0b6d7e78b
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-05-30 11:16:22 +0900

    [Bronze IV] Title: 빵, Time: 48 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+min_time = 1001
+for _ in range(N):
+    A, B = map(int, input().split())
+
+    if A > B:
+        continue
+
+    min_time = min(min_time, B)
+
+print(min_time if min_time != 1001 else -1)
