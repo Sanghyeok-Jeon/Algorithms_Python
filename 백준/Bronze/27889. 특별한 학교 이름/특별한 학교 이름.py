fabd84ef4eb7b16a25dd0736e45caa28b5458c28
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-05-30 11:18:17 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,9 @@
+short_name = input()
+if short_name == 'NLCS': 
+    print('North London Collegiate School')
+elif short_name == 'BHA': 
+    print('Branksome Hall Asia')
+elif short_name == 'KIS': 
+    print('Korea International School')
+else:
+    print('St. Johnsbury Academy')
