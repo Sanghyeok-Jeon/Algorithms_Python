df6f0955583b196cad85292ddb40b2f608077ea0
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-05-30 10:59:31 +0900

    [Silver III] Title: N과 M (3), Time: 1072 ms, Memory: 169824 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+def DFS(cnt):
+    if cnt == M:
+        print(*ans)
+        return
+
+    for i in range(1, N+1):
+        ans.append(i)
+        DFS(cnt+1)
+        ans.pop()
+
+N, M = map(int, input().split())
+
+ans = []
+DFS(0)
