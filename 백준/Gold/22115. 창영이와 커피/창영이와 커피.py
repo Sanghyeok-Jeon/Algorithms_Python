8a52acd7cc3104a22b6d1d451d6afa0ab3f1ae34
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-06-11 08:39:32 +0900

    [Gold V] Title: 창영이와 커피, Time: 128 ms, Memory: 110024 KB -BaekjoonHub

--- 백준/Gold/22115. 창영이와 커피/창영이와 커피.py
+++ 백준/Gold/22115. 창영이와 커피/창영이와 커피.py
@@ -5,16 +5,17 @@ N, K = map(int, input().split())
 caffeine_levels = list(map(int, input().split()))
 
 # dp[x]는 카페인 x를 얻기 위해 필요한 최소 커피 개수
-dp = [float('inf')] * (K + 1)
+dp = [101] * (100000 + 1)
 dp[0] = 0  # 0mg 카페인을 얻기 위해 필요한 커피는 0개
 
+length = 0
 for caffeine in caffeine_levels:
-    for j in range(K, caffeine - 1, -1):
-        if dp[j - caffeine] != float('inf'):
-            dp[j] = min(dp[j], dp[j - caffeine] + 1)
+    for j in range(length + caffeine, caffeine - 1, -1):
+        dp[j] = min(dp[j], dp[j - caffeine] + 1)
+    length += caffeine
 
 # 결과 출력
-if dp[K] == float('inf'):
+if dp[K] == 101:
     print(-1)
 else:
     print(dp[K])
