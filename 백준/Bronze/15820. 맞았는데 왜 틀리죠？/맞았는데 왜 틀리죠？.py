3bae61ef4004879636ac447766e780251bd96447
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-08-01 10:02:52 +0900

    [Bronze III] Title: 맞았는데 왜 틀리죠?, Time: 44 ms, Memory: 31120 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Bronze/15820. 맞았는데 왜 틀리죠？/맞았는데 왜 틀리죠？.py
@@ -0,0 +1,16 @@
+S1, S2 = map(int, input().split())
+
+answer = 'Accepted'
+for _ in range(S1):
+    sample_answer, my_answer = map(int, input().split())
+    if sample_answer != my_answer:
+        answer = 'Wrong Answer'
+        break
+
+for _ in range(S2):
+    system_answer, my_answer = map(int, input().split())
+    if system_answer != my_answer:
+        answer = 'Why Wrong!!!'
+        break
+
+print(answer)
