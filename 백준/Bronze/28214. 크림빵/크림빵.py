705d8dd4c3bf2e5cc072428a681cfd1e64bd61bd
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-08-03 20:57:23 +0900

    [Bronze III] Title: 크림빵, Time: 32 ms, Memory: 31120 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -0,0 +1,9 @@
+N, K, P = map(int, input().split())
+breads = list(map(int, input().split()))
+
+cnt = 0
+for i in range(0, len(breads), K):
+    if breads[i:i + K].count(1) >= P:
+        cnt += 1
+
+print(cnt)
