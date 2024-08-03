a7eb1cb4ad4b47b3e78ba48ff67feda244838c4e
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-08-03 20:59:55 +0900

    [Bronze III] Title: 크림빵, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -3,7 +3,7 @@ breads = list(map(int, input().split()))
 
 cnt = 0
 for i in range(0, len(breads), K):
-    if breads[i:i + K].count(1) >= P:
+    if breads[i:i + K].count(0) < P:
         cnt += 1
 
 print(cnt)
