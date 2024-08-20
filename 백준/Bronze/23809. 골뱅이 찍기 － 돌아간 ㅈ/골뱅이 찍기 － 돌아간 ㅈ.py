a3ff0256a28c3671ccd3938e8731e4aa7516a64d
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-08-21 08:48:52 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅈ, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23809. 골뱅이 찍기 － 돌아간 ㅈ/골뱅이 찍기 － 돌아간 ㅈ.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+for i in range(N):
+    print('@' * N + ' ' * N * 3 + '@' * N)
+
+for i in range(N):
+    print('@' * N + ' ' * N * 2 + '@' * N)
+
+for i in range(N):
+    print('@' * N * 3)
+
+for i in range(N):
+    print('@' * N + ' ' * N * 2 + '@' * N)
+
+for i in range(N):
+    print('@' * N + ' ' * N * 3 + '@' * N)
