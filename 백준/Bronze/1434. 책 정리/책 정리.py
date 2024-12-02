318ee85ce365056b4e6df74ab7e2f7c89005256b
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-12-03 08:50:38 +0900

    [Bronze II] Title: 책 정리, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1434. 책 정리/책 정리.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+i = 0
+j = 0
+while i < N and j < M:
+    if A[i] >= B[j]:
+        A[i] -= B[j]
+        j += 1
+    else:
+        i += 1
+
+print(sum(A))
