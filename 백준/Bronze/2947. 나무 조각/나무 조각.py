86bd950a216a20f49734fbaa5d7164724fcaca64
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2024-12-10 08:42:02 +0900

    [Bronze I] Title: 나무 조각, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -0,0 +1,21 @@
+wood = list(map(int, input().split()))
+
+while True:
+    if wood == [1, 2, 3, 4, 5]:
+        break
+
+    if wood[0] > wood[1]:
+        wood[0], wood[1] = wood[1], wood[0]
+        print(*wood)
+
+    if wood[1] > wood[2]:
+        wood[1], wood[2] = wood[2], wood[1]
+        print(*wood)
+
+    if wood[2] > wood[3]:
+        wood[2], wood[3] = wood[3], wood[2]
+        print(*wood)
+
+    if wood[3] > wood[4]:
+        wood[3], wood[4] = wood[4], wood[3]
+        print(*wood)
