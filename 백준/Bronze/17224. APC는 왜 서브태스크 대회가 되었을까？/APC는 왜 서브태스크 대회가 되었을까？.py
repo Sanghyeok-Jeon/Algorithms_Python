dba001ddeef444a27acb90057abd1fa9bcb15889
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2025-01-10 09:06:26 +0900

    [Bronze I] Title: APC는 왜 서브태스크 대회가 되었을까?, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.py
@@ -0,0 +1,20 @@
+N, L, K = map(int, input().split())
+problems = [list(map(int, input().split())) for _ in range(N)]
+
+problems.sort(key=lambda x:(x[1], x[0]))
+score = 0
+solved = 0
+for i in range(N):
+    sub1, sub2 = problems[i]
+
+    if sub1 <= L:
+        score += 100
+        if sub2 <= L:
+            score += 40
+
+        solved += 1
+
+    if solved == K:
+        break
+
+print(score)
