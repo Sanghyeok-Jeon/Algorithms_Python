6d2bbff4426e80d72dbe4cb7a3525357aa663bf9
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2025-02-24 09:06:50 +0900

    [Silver V] Title: 칠무해, Time: 3500 ms, Memory: 271496 KB -BaekjoonHub

--- 백준/Silver/14729. 칠무해/칠무해.py
+++ 백준/Silver/14729. 칠무해/칠무해.py
@@ -2,7 +2,9 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-score = sorted([float(input()) for _ in range(N)])
+score = [float(input()) for _ in range(N)]
+
+score.sort()
 
 for i in range(7):
     print('{:.3f}'.format(score[i]))
