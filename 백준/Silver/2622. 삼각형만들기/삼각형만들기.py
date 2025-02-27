0d2f5f508d385357b88474fcae49bbcf366e04e2
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2025-02-27 09:42:46 +0900

    [Silver V] Title: 삼각형만들기, Time: 808 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+cnt = 0
+for a in range(1, N // 3 + 1):
+    for b in range(a, (N - a) // 2 + 1):
+        c = N - (a + b)
+        if c < b:
+            break
+        if a + b > c:
+            cnt += 1
+
+print(cnt)
