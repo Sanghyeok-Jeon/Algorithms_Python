62700561b578c3b99a020e3a4df6d05c7dddc093
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2025-12-05 08:45:31 +0900

    [Silver III] Title: 가희와 키워드, Time: 728 ms, Memory: 58012 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+keywords = {input().strip() for _ in range(n)}
+
+for _ in range(m):
+    memo = set(input().strip().split(','))
+    keywords.difference_update(memo)
+    print(len(keywords))
