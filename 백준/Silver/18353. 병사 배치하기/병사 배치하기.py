703b726b195b96bed47e0d9b74034857cac6ed13
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2026-03-23 09:55:53 +0900

    [Silver II] Title: 병사 배치하기, Time: 620 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# 뒤집어서 LIS 구하기
+arr.reverse()
+
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
