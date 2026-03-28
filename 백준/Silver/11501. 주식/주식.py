1d7b81af1dc5606868f569886f7b396814f5d4e9
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2026-03-28 22:57:58 +0900

    [Silver II] Title: 주식, Time: 3068 ms, Memory: 159996 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    prices = list(map(int, input().split()))
+    
+    max_price = 0
+    profit = 0
+    
+    for i in range(n - 1, -1, -1):
+        if prices[i] > max_price:
+            max_price = prices[i]
+        else:
+            profit += max_price - prices[i]
+    
+    print(profit)
