becdbd514f87f6f0565f33315cb0b12fd754e517
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2026-04-14 15:25:27 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 252 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+p = [0] + list(map(int, input().split()))
+
+dp = [10**9] * (n + 1)
+dp[0] = 0
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = min(dp[i], dp[i - j] + p[j])
+
+print(dp[n])
