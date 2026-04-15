2b7df7aac5e6ddda10173506c3fe571d91f487a7
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2026-04-16 08:59:43 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 48 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -4,16 +4,17 @@ input = sys.stdin.readline
 
 s = input().strip()
 q = int(input())
+n = len(s)
 
-prefix = [[0] * 26 for _ in range(len(s) + 1)]
+prefix = [[0] * (n + 1) for _ in range(26)]
 
-for i in range(len(s)):
-    for j in range(26):
-        prefix[i + 1][j] = prefix[i][j]
-    prefix[i + 1][ord(s[i]) - ord('a')] += 1
+for i in range(n):
+    for c in range(26):
+        prefix[c][i + 1] = prefix[c][i]
+    prefix[ord(s[i]) - ord('a')][i + 1] += 1
 
 for _ in range(q):
     a, l, r = input().split()
     l, r = int(l), int(r)
     idx = ord(a) - ord('a')
-    print(prefix[r + 1][idx] - prefix[l][idx])
+    print(prefix[idx][r + 1] - prefix[idx][l])
