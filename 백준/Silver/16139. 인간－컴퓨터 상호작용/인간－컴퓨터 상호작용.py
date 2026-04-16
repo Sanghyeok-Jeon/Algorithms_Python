132a690b30038d7fb7def2ec4654031004c7fd18
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2026-04-16 09:00:32 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 596 ms, Memory: 43140 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,20 +1,20 @@
 import sys
+from bisect import bisect_left, bisect_right
 
 input = sys.stdin.readline
 
 s = input().strip()
 q = int(input())
-n = len(s)
 
-prefix = [[0] * (n + 1) for _ in range(26)]
+pos = [[] for _ in range(26)]
 
-for i in range(n):
-    for c in range(26):
-        prefix[c][i + 1] = prefix[c][i]
-    prefix[ord(s[i]) - ord('a')][i + 1] += 1
+for i, ch in enumerate(s):
+    pos[ord(ch) - ord('a')].append(i)
 
 for _ in range(q):
     a, l, r = input().split()
-    l, r = int(l), int(r)
-    idx = ord(a) - ord('a')
-    print(prefix[idx][r + 1] - prefix[idx][l])
+    l = int(l)
+    r = int(r)
+
+    arr = pos[ord(a) - ord('a')]
+    print(bisect_right(arr, r) - bisect_left(arr, l))
