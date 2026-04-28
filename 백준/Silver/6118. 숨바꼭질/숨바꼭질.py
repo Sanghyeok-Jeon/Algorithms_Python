2a9905ee50050b8a06278726a89f37c7ad900b8a
Sanghyeok-Jeon <61657839+Sanghyeok-Jeon@users.noreply.github.com>
2026-04-28 10:25:24 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 39628 KB -BaekjoonHub

---
+++ 백준/Silver/6118. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+dist = [-1] * (n + 1)
+dist[1] = 0
+
+q = deque([1])
+
+while q:
+    cur = q.popleft()
+
+    for nxt in graph[cur]:
+        if dist[nxt] == -1:
+            dist[nxt] = dist[cur] + 1
+            q.append(nxt)
+
+max_dist = max(dist)
+barn_num = dist.index(max_dist)
+count = dist.count(max_dist)
+
+print(barn_num, max_dist, count)
